ae6d7b4e5ab5420136833a3a5c6e8fc6aaca1353
Mathieu <mathieu.linder.dev@gmail.com>
2021-06-23 16:58:26 +0200

    Appel de l'api et style

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div className="about">
+            <Navigation/>
+            <Logo/>
+            <h1>À propos</h1>
+            <br/>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit officia ducimus ad qui voluptate? Culpa, sunt dicta ipsum eligendi mollitia ea atque cum. Error, voluptate deleniti, voluptates repellendus consectetur neque atque delectus quas sint laudantium, aperiam temporibus cum consequuntur esse harum inventore? Maiores illo unde, accusantium, reiciendis dolore aspernatur fugiat libero debitis quisquam ipsum excepturi blanditiis. Hic fugiat provident cum.</p>
+            <br/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit perspiciatis dolore laborum! Accusantium, iure impedit! Recusandae omnis iusto ipsa tempora nostrum blanditiis nam officiis. Est sed illo magnam. Atque repellat laborum doloribus sapiente accusamus sit in possimus laudantium, tenetur repudiandae dolorum odit inventore molestias voluptas eum delectus veniam hic. Explicabo doloremque nesciunt facere necessitatibus optio doloribus cumque rem. Doloribus sunt nesciunt vitae aperiam? Adipisci, aliquid. Rerum dolor ratione, beatae consequuntur consequatur fugit pariatur assumenda, nobis tempora nulla illo impedit iusto.</p>
+        </div>
+    );
+};
+
+export default About;
